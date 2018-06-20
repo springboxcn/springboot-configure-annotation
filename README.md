03505a92874a1c37204b228752b5788d7f40a62c
maskleo <masque@aliyun.com>
2018-06-20 16:04:11 +0800

    将配置与实体绑定

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # springboot-configure-annotation
+
+EnvironmentManager
+
+https://spring.io/guides/gs/centralized-configuration/
+https://www.devglan.com/spring-cloud/refresh-property-config-runtime
+https://www.oodlestechnologies.com/blogs/Reload-External-Configuration-on-Runtime-in-Spring-Boot
+
+
+
+https://stackoverflow.com/questions/49311068/refreshscope-and-refresh-not-working
+
+https://stackoverflow.com/questions/42441506/refresh-a-bean-without-configuration-change-with-refreshscope
+
+https://github.com/spring-cloud/spring-cloud-config/issues/290
+
+https://stackoverflow.com/questions/33365874/how-to-update-configuration-of-springboot-application-at-runtime-without-reloadi
+
+[spring boot 配置动态刷新](http://www.cnblogs.com/flying607/p/8459397.html)
+
+https://www.jianshu.com/p/7acefd689b6f
+
+http://www.scienjus.com/spring-cloud-refresh/
+
+https://www.safaribooksonline.com/library/view/building-microservices-with/9780134192468/02_08.html
--- demo2/src/main/java/edu/maskleo/configure/demo2/demo/TestController.java
+++ demo2/src/main/java/edu/maskleo/configure/demo2/demo/TestController.java
@@ -1,6 +1,9 @@
 package edu.maskleo.configure.demo2.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,15 @@ public class TestController {
     @Autowired
     private Teacher teacher;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("test")
     public ResponseEntity<String> test(){
+        //Student student = environment.getProperty("student.jack", Student.class);
+        System.out.println(environment.getProperty("student.jack.name"));
+        BindResult<Student> student = Binder.get(environment).bind("student.jack" ,Student.class);
+        System.out.println(String.format("key:student.jack,value:%s",student.get()));
         return ResponseEntity.ok(String.format("jack:%s,marry:%s,tom:%s",teacher.getJack(),teacher.getMarry(),teacher.getTom()));
     }
 
